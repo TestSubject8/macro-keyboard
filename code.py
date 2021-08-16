197aebbb9d06f3288757b4fa6f6d27561aa173b7
TestSubject_8 <agkatreya@gmail.com>
2021-08-16 14:16:03 +0530

    Created new framework where all actual work done is specified in code.py

--- code.py
+++ code.py
@@ -5,13 +5,6 @@ from adafruit_hid.consumer_control_code import ConsumerControlCode
 
 m = menu.Menu()
 
-def scroll(kbd, _, chg):
-	if chg < 0:
-		for i in range(-chg):
-			kbd.send(Keycode.UP_ARROW)
-	elif chg > 0:
-		for i in range(chg):
-			kbd.send(Keycode.DOWN_ARROW)
 
 def vol(_, cc, chg):
 	print('called')
@@ -31,10 +24,34 @@ def bright(_, cc, chg):
 		for i in range(chg):
 			cc.send(ConsumerControlCode.BRIGHTNESS_INCREMENT)
 
+# SCROLL Menu Item
 
-items.Keyboard(m, 'VOL', vol)
-items.Keyboard(m, 'BRI', bright)
-items.Keyboard(m, 'SCR', scroll)
+def scroll_action(menu):
+	chg = menu.encoder.position - menu.last_position
+	if chg < 0:
+		if menu.button_rot.value:
+			code = Keycode.UP_ARROW
+		else:
+			code = Keycode.LEFT_ARROW
+		for i in range(-chg):
+			menu.kbd.send(code)
 
+	elif chg > 0:
+		if menu.button_rot.value:
+			code = Keycode.DOWN_ARROW
+		else:
+			code = Keycode.RIGHT_ARROW
+		for i in range(chg):
+			menu.kbd.send(code)
+	menu.last_position += chg
 
-m.mainloop()
+def scroll_disp(menu):
+	menu.oled.text('Next time then', 20,20, None)
+
+items.Item(m, scroll_disp, scroll_action)
+
+# ensure screensaver is added first, make some timer to go back to index = 0
+# items.Keyboard(m, 'VOL', vol)
+# items.Keyboard(m, 'BRI', bright)
+# items.Keyboard(m, 'SCR', scroll)
+m.next()
--- items.py
+++ items.py
@@ -1,20 +1,40 @@
 import time
+import microcontroller
 
-class Keyboard:
-    def __init__(self, menu, label, handler):
+class Item:
+    def __init__(self, menu, disp_func, action_func):
         self.menu = menu
-        self.handler = handler
-        self.label = label  
-        self.indicator = 0
         self.menu.items.append(self)
+        self.disp_func = disp_func
+        self.action_func = action_func
 
-    def rot_func(self, chg):
-        self.handler(self.menu.kbd, self.menu.cc, chg)
-        self.indicator = chg
+    def disp(self):
+        o = self.menu.oled
+        o.fill(0)
+        # Time / countdown
+        _, _, _, hour, minute, second, _, _, _ = self.menu.rtc.datetime
+        time_string = '%02d'%(hour) + ':' + '%02d'%(minute)
+        # + ':' + '%02d'%(second)
+        o.text(time_string, 0,0, None)
 
-    def show(self, oled):
-        if self.indicator < 0:
-            oled.text('<-', 50,10, None)
-        elif self.indicator > 0:
-            oled.text('->', 50,10, None)
-        self.indicator = 0
+        # FPS / Temp
+        o.text(str(self.menu.prev_round_trip)[:4]+'s', 53, 0, None)
+        o.text(str(microcontroller.cpu.temperature)[:4] + ' C', 85,0, None)
+        for i in range(0,10):
+            o.pixel(51,i,1) 
+            o.pixel(83,i,1) 
+
+        # active label and custom display
+        # self.oled.text(self.items[self.active].label, 38,10, None)
+
+        self.disp_func(self.menu)
+        o.show()
+
+
+    def mainloop(self):
+        while True:
+            stime = time.monotonic()
+            self.action_func(self.menu)
+            self.disp()
+            self.menu.prev_round_trip = time.monotonic() - stime
+        
