a50b337f0f6581b3baef5a01671b28d304927cc1
TestSubject8 <agkatreya@gmail.com>
2022-06-22 14:41:14 +0100

    Working connections and spi oled device. Simple menuing and configs

---
+++ controller.py
@@ -0,0 +1,71 @@
+import luma_oled
+import os
+import time
+
+def set_border(draw, opts):
+  draw.rectangle(luma_oled.device.bounding_box, outline='white', fill='black')
+
+def write_text(draw, opts):
+  # print(opts)
+  draw.rectangle(luma_oled.device.bounding_box, outline='white', fill='black')
+  draw.multiline_text((50,30),opts, align='center', fill='white')
+
+def dot(draw, opts):
+  x = int(opts[0])
+  y = int(opts[1])
+
+  size = 3
+  for i in range(size+1):
+    for j in range(size+1):
+      draw.point((x+i,y+j), fill='white')
+
+def pixel_bounce():
+  x = 0
+  y = 0
+  dx = 1.3
+  dy = 1
+  limit = 128
+# for i in range(3):
+  while(True):
+    if y >= 64: 
+      dy = -1
+      border_y = 0
+    if y <= 0: 
+      dy = 1
+      border_y = 64
+    if x <= 0: 
+      dx = 1.3
+      border_x = 128
+    if x >= 128:
+      dx = -1.3
+      border_y = 0
+    x += dx
+    y += dy
+    luma_oled.frame(dot,(x,y),1/60)
+    
+
+  for i in range(3):
+    delta = [1,1,-1,-1]
+    border = [128,64,0,0]
+    while(x<border[0]):
+      x += delta[0]
+      y += delta[1]
+      if x == border[0]:
+        delta[0] = -1
+        border[0] = 0
+      if y == border[1]:
+        delta[1] = -1
+        border[1] = 0
+      luma_oled.frame(dot,(x,y),1/10)
+    luma_oled.device.clear() 
+
+def temp(draw, args):
+  draw.text((10,10),cpu_temp)
+
+while(True):
+  # luma_oled.frame(write_text,("Finally did it \n Found the problem \n       and fixed it \n Cost just sanity"),10)
+  #luma_oled.frame(pixel_bounce)
+  #pixel_bounce()
+
+  cpu_temp = os.popen("vcgencmd measure_temp").readline().replace("temp=", "")
+  luma_oled.frame(write_text, cpu_temp, 1)
---
+++ luma_oled.py
@@ -0,0 +1,38 @@
+import time
+from luma.core.interface.serial import spi
+from luma.core.render import canvas
+from luma.oled.device import sh1106
+import os
+
+serial = spi(device=0, port=0) #, gpio_DC=6,gpio_RST=4,gpio_CS=5)
+device = sh1106(serial, rotate=2)
+
+
+def frame(callback, args=None, sleep_time=0):
+  with canvas(device) as draw:
+    callback(draw, args)
+  time.sleep(sleep_time)
+  device.clear()
+
+#while True:
+#  x = 2
+#  y = 2
+#  offset = 0
+#  with canvas(device) as draw:
+#  # draw.rectangle(device.bounding_box, outline="white", fill="black")
+#    message = ["You are a genius","immortal who deserves","their high head"]
+#    for i in range(len(message)):
+#      draw.text((x, y+i*12), message[i], fill="white")
+#  time.sleep(2)
+#  print('done')
+#    
+#  device.clear()
+#  points = []
+#  for i in range(128):
+#    for j in range(64):
+#      with canvas(device) as draw:
+#        points.append((i,j))
+#        draw.point(points, fill='white')
+#
+#  time.sleep(2)
+#  print('done')
